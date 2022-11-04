324c148ddf27fa49896548d755805e047adee684
Adam Musial <adammusial@wp.pl>
2022-11-04 23:52:56 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Cześć");
+
+let button = document.querySelector(".pricing__button");
+let table = document.querySelector(".pricing__table")
+let buttonInnerText = document.querySelector(".pricing__buttonInnerText")
+
+
+button.addEventListener("click", () => {
+    table.classList.toggle("pricing__tableHigh")
+    buttonInnerText.innerText = table.classList.contains("pricing__tableHigh") ? "Pomniejsz " : "Powiększ ";
+});
