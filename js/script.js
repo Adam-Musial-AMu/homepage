c10b35e63eb3a96288c7cfcbe771e91915f10020
Adam Musial <adammusial@wp.pl>
2022-11-27 18:07:05 +0100

    JS into arrow functions converted

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-console.log("Witaj w świecie wizualizacji");
+{
+    const welcome = () => {
+        console.log("Witaj w świecie wizualizacji");
+    }
 
-let button = document.querySelector(".pricing__button");
-let table = document.querySelector(".pricing__table")
-let buttonInnerText = document.querySelector(".pricing__buttonInnerText")
+    const changeTableFontSize = () => {
+        const table = document.querySelector(".pricing__table")
+        const buttonInnerText = document.querySelector(".pricing__buttonInnerText")
 
+        table.classList.toggle("pricing__tableHigh")
+        buttonInnerText.innerText = table.classList.contains("pricing__tableHigh") ? "Pomniejsz " : "Powiększ ";
+    }
 
-button.addEventListener("click", () => {
-    table.classList.toggle("pricing__tableHigh")
-    buttonInnerText.innerText = table.classList.contains("pricing__tableHigh") ? "Pomniejsz " : "Powiększ ";
-});
+    const init = () => {
+        const button = document.querySelector(".pricing__button");
+        button.addEventListener("click", changeTableFontSize)
+
+        welcome();
+    }
+
+    init()
+
+}
